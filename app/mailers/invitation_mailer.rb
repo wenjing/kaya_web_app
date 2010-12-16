7e62e6f56ca413ea3d187ebb58be6b9547572422
Wenjing Chu <chu.wenjing@gmail.com>
2010-12-15 16:19:30 -0800

    Try again with the mailer of heroku version

--- app/mailers/invitation_mailer.rb
+++ app/mailers/invitation_mailer.rb
@@ -1,12 +1,11 @@
 class InvitationMailer < ActionMailer::Base
-  default :from => "no-reply@kaya-labs.com"
 
   def signup_invitation (user)
-    @user = user
-    @url = "http://www.kayameet.com"
-    mail (:to => user.email,
-          :bcc => "wenjing.chu@kaya-labs.com",
-          :subject => "You've been invited to a Kaya Meet")
+    recipients "#{user.name} <#{user.email}>"
+    from      "no-reply@kaya-labs.com"
+    subject   "You are invited to a Kaya Meet!"
+    sent_on   Time.now
+    body      {:user => user, :url => "http://www.kayameet.com"}
   end
 
 end
