934f9d8b53ebe05118f561d477d1654af1927485
Wenjing Chu <chu.wenjing@gmail.com>
2010-12-15 16:39:06 -0800

    Remove body which cause new version of mailer to crash

--- app/mailers/invitation_mailer.rb
+++ app/mailers/invitation_mailer.rb
@@ -1,11 +1,21 @@
+
 class InvitationMailer < ActionMailer::Base
 
-  def signup_invitation (user)
-    recipients "#{user.name} <#{user.email}>"
-    from      "no-reply@kaya-labs.com"
-    subject   "You are invited to a Kaya Meet!"
-    sent_on   Time.now
-    body      {:user => user, :url => "http://www.kayameet.com"}
+  default :from => "notifications@example.com"
+   
+  def signup_invitation(user)
+    @user = user
+    @url  = "http://www.kayameet.com"
+    mail(:to => user.email,
+         :bcc => "wenjing.chu@kaya-labs.com",
+        :subject => "You've been invited to a Kaya Meet")
   end
+#  def signup_invitation (user)
+#    recipients "#{user.name} <#{user.email}>"
+#    from      "no-reply@kaya-labs.com"
+#    subject   "You are invited to a Kaya Meet!"
+#    sent_on   Time.now
+#    body      {:user => user, :url => "http://www.kayameet.com"}
+#  end
 
 end
