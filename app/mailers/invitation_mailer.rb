d28d08d9e0bc57b3f3190b6dc3c51ce476b9e0a8
Wenjing Chu <chu.wenjing@gmail.com>
2010-12-15 16:00:17 -0800

    Fixed a typo

--- app/mailers/invitation_mailer.rb
+++ app/mailers/invitation_mailer.rb
@@ -1,11 +1,12 @@
 class InvitationMailer < ActionMailer::Base
   default :from => "no-reply@kaya-labs.com"
 
-  def signup_invitation (user, recipents)
+  def signup_invitation (user, recipients)
     @user = user
+    @recipients = recipients
     @url = "www.kayameet.com"
-    mail (:to => recipients,
-          :bcc => wenjing.chu@kaya-labs.com,
+    mail (:to => @recipients,
+          :bcc => "wenjing.chu@kaya-labs.com",
           :subject => "You've been invited to a Kaya Meet")
   end
 
