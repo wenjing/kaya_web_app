6bd821de3a562363b2259cc19925f5a126b9fa39
hongzhao <hongzhao@kaya-labs.com>
2011-02-02 22:27:36 -0800

    heroku db error

--- db/migrate/20110125155037_change_chatters_content_to_text.rb
+++ db/migrate/20110125155037_change_chatters_content_to_text.rb
@@ -1,13 +1,13 @@
 class ChangeChattersContentToText < ActiveRecord::Migration
   def self.up
     change_table :chatters do |t|
-      t.change :content, :text, :limit=>500
+      t.change :content, :text
     end
   end
 
   def self.down
     change_table :chatters do |t|
-      t.change :content, :text, :limit=>250
+      t.change :content, :text
     end
   end
 end
